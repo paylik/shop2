67bd1475e7ea7bc1a6a69bc4016e9ed72dc6380e
paylik <paylik@yandex.ru>
2017-02-22 14:18:51 +0300

    +video

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -12,11 +12,16 @@ export default class ContactPage extends React.Component {
                 <h2>ТОРГОВЫЙ ОБЪЕКТ 'ЖЕНАВИ' </h2>
                     <h3>РЕЖИМ РАБОТЫ: <br />
                         пн-пт 10<sup>00</sup>-20<sup>00</sup>, сб-вс 10<sup>00</sup>-19<sup>00</sup>, перерыв 14<sup>00</sup>-15<sup>00</sup></h3>
+                    <p>Частноу торговое унитарное предприятие "Ритакар"</p>
+                    <p>УНП 591804844, г. Сморгонь, ул. Ленина д. 4</p>
+                    <p>р/с 3012164648018 в дополнительном офисе №404 г.Сморгонь ОАО «БПС-Сбербанк»</p>
                     <p>Директор Ольга Николаевна +375-29-782-33-37 (мтс) </p>
                     <p>Наш магазин находится по адресу: г. Сморгонь, ул. Ленина д. 4</p>
-                <Link src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=KBhaELBmF3spV-HCmTpe8Qnrt2dnYEkm&amp;width=627&amp;height=620&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"
-                >
-                </Link>    
+
+                <div>
+                    <iframe width={425} height={350} frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0} src="http://www.openstreetmap.org/export/embed.html?bbox=26.400870680809025%2C54.48021471265457%2C26.40269458293915%2C54.48155952127111&layer=mapnik" style={{border: '1px solid black'}} /><br /><small><a href="http://www.openstreetmap.org/#map=19/54.48089/26.40178&layers=N">Посмотреть более крупную карту</a></small>
+                </div>
+
                 <p>Ориентир: Кафе "Вместе", цокольный этаж, вход с правой стороны</p>
                     <img src="img/kafe.jpg" width={1100} height={733} />
                     <img src="img/entrance.jpg" width={1100} height={1239} />
--- src/pages/video/video.js
+++ src/pages/video/video.js
@@ -6,8 +6,14 @@ export default class VideoPage extends React.Component {
 
     render () {
         return (
-            <div>
-                <p>video</p>
+            <div className='video'>
+
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/c-UusSR_fzw" frameBorder={0} allowFullScreen />
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/2FrsHEA6wsQ" frameBorder={0} allowFullScreen />
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/xfNsRlfpEJA" frameBorder={0} allowFullScreen />
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/zqcxHNUPsA8" frameBorder={0} allowFullScreen />
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/GNcuxs0t6pU" frameBorder={0} allowFullScreen />
+                <iframe width={560} height={315} src="https://www.youtube.com/embed/2ECYHveLyzk" frameBorder={0} allowFullScreen />
             </div>
         );
     }
